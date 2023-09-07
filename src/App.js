9ba2461272272fe93bc98f60e553832eebc75942
wnsghks <wnsghks0223@gmail.com>
2023-09-08 00:39:20 +0900

    add frame

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import "./style/App.css";
+//npm install react-router-dom 설치 후 사용 가능
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Reset } from "styled-reset";
 
 function App() {
   return (
     <div className="App">
+      <Reset />
+      
       <h1>hi</h1>
     </div>
   );
