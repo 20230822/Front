9bd00d63df91ef1b631f13e813cd5c88c3d96ad8
wnsghks <wnsghks0223@gmail.com>
2023-09-10 18:46:41 +0900

    add header.css

--- src/routes/Header.js
+++ src/routes/Header.js
@@ -16,24 +16,24 @@ function Header() {
         <Light />
       </div>
       
-      <div className="menu-top">
-        <h1><Link className="menu-title" to={"/"}>Light Mall</Link></h1>
-        <ul>
-          <li>로그인</li> {/* 로그인 시 로그아웃으로 */}
-          <li>마이페이지</li>
-          <li>고객센터</li>
-        </ul>
-      </div>
-      
-      <div className="menu-bottom">
-        <nav className="menu-category">
-          <span>펜던트</span>
-          <span>플로어 램프</span>
-          <span>테이블 램프</span>
-          <span>월 램프</span>
-        </nav>
+      <div className="menu">
+        <div className="menu-top">
+          <h1><Link className="menu-title" to={"/"}>Light Mall</Link></h1>
+          <ul className="menu-help">
+            <li className="menu-help-item">로그인</li> {/* 로그인 시 로그아웃으로 */}
+            <li className="menu-help-item">마이페이지</li>
+            <li className="menu-help-item">고객센터</li>
+          </ul>
+        </div>
+        
+        <div className="menu-bottom">
+          <nav>
+            <span className="menu-category-item">펜던트</span>
+            <span className="menu-category-item">플로어 램프</span>
+            <span className="menu-category-item">테이블 램프</span>
+            <span className="menu-category-item">월 램프</span>
+          </nav>
 
-        <div className="menu-research">
           <Search />
         </div>
       </div>
