532d54e0d6e9349f4f258f2d580d568d90033ec1
howky <howeorb@naver.com>
2023-09-10 21:40:39 +0900

    로그인 수정

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -3,20 +3,23 @@ import { Reset } from "styled-reset";
 import "../style/Login.css";
 function Login() {
   return (
-    <div className="login-container">
-      <Reset />
-      <h2>SIGN IN</h2>
-      <div className="input-group">
-        <label value="username">ID:</label>
-        <input type="text" id="username" placeholder="Enter your username" />
+    <div className="login">
+      <div className="login-container">
+        <Reset />
+        <h2>SIGN IN</h2>
+        <div className="input-group">
+          <label value="username"></label>
+          <input type="text" id="username" placeholder="ID" />
+          <br></br>
+          <label value="password"></label>
+          <input type="password" id="password" placeholder="PW" />
+          <br></br>
+          <input type="submit" value="Login"/>
+        </div>
       </div>
-      <div className="input-group">
-        <label value="password">PW:</label>
-        <input type="password" id="password" placeholder="Enter your password" />
-      </div>
-      <input type="submit" value="Login"/>
     </div>
   );
 }
+
 
 export default Login;
