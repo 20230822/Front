4e28afba7ba72aafe18bb1e43d0d7f1eccaaabac
howky <howeorb@naver.com>
2023-09-11 19:27:25 +0900

    로그인 회원가입 수정 완 다음 작업 회원가입 정보 로그인으로 넘기기

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,47 +1,69 @@
 import React, { useState } from "react";
+import { Reset } from "styled-reset";
 import { Link } from "react-router-dom";
 import "../style/Login.css";
 
 function Login() {
-  const [userID, setUsername] = useState(""); // 초기값은 빈 문자열
-  const handleUsernameChange = (e) => {
-    setUsername(e.target.value); // 입력된 값으로 상태(State) 업데이트
-  };
-  const [password, setPassword] = useState(""); // 초기값은 빈 문자열
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value); // 입력된 값으로 상태(State) 업데이트
+  const [formData, setFormData] = useState({
+    userID: "",
+    password: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const { userID, password, userName } = formData;
+  };
+  
   return (
     <div className="login">
       <div className="box"></div>
       <div className="login-container">
+        <Reset />
         <h2>SIGN IN</h2>
+        <form onSubmit={handleSubmit}>
         <div className="input-group">
-        <label htmlFor="userID">User ID</label>
-          <input type="text" id="userID" placeholder="ID"
-            value={userID} // 상태(State)에 저장된 값 사용
-            onChange={handleUsernameChange} // 입력 값 변경 이벤트 핸들러 연결
-          />
-          <br />
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" placeholder="PW" 
-          value={password}
-          onChange={handlePasswordChange}/>
-          <br />
+        <label htmlFor="userID">UserID</label>
+            <input
+              type="text"
+              id="userID"
+              name="userID"
+              placeholder="ID"
+              value={formData.userID}
+              onChange={handleInputChange}
+            />
+            <br />
+            <label htmlFor="password">Password</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="PW"
+              value={formData.password}
+              onChange={handleInputChange}
+            />
+            <br />
           <input type="submit" value="Login" />
           <br />
           <Link className="Sign" to={"/Signup"}>
             Sign up
           </Link>
         </div>
+        </form>
+      </div>
+      {/*<div>
+        <p>Username: {formData.userID}</p>
       </div>
       <div>
-        <p>Username: {userID}</p>
-      </div>
-      <div>
-        <p>password: {password}</p>
-      </div>
+        <p>password: {formData.password}</p>
+  </div>*/}
     </div>
   );
 }
