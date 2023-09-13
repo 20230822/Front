24cd761fb5b0d2eeea3aa868db076d2a26f18ea3
howky <howeorb@naver.com>
2023-09-13 22:57:48 +0900

    메인화면 검색창

--- src/components/search.js
+++ src/components/search.js
@@ -1,7 +1,23 @@
+import React, { useState } from "react";
+import "../style/search.css";
+
 function Search() {
+  const [isFormVisible, setIsFormVisible] = useState(false);
+
+  const toggleFormVisibility = () => {
+    setIsFormVisible(!isFormVisible);
+  };
+
+  // 폼이 보이는 경우에만 CSS 클래스 추가
+  const formClass = isFormVisible ? "show" : "none";
+
   return (
-    <div className="search">
-      <h1>search</h1>
+    <div className="search icon" onClick={toggleFormVisibility}>
+      search
+      <form action="#" method="get" className={formClass} onClick={(e) => e.stopPropagation()}>
+        <input type="text" onClick={(e) => e.stopPropagation()} />
+        <input type="submit" value={"검색"} className="search icon" onClick={(e) => e.stopPropagation()} />
+      </form>
     </div>
   );
 }
