9fc61b6cea76d40e08a2df7b9567a65862081d53
wnsghks <wnsghks0223@gmail.com>
2023-10-10 17:42:27 +0900

    light illust

--- src/components/search.js
+++ src/components/search.js
@@ -14,8 +14,8 @@ function Search() {
   return (
     <div className="search icon" onClick={toggleFormVisibility}>
       <form action="#" method="get" className={formClass} onClick={(e) => e.stopPropagation()}>
-        <input type="text" className="searchbox" onClick={(e) => e.stopPropagation()} />
-        <input type="submit" className="search icon " onClick={(e) => e.preventDefault()} />
+        <input type="text" className="search-box" onClick={(e) => e.stopPropagation()} />
+        <input type="submit" className="search icon" onClick={(e) => e.preventDefault()} />
       </form>
     </div>
   );
