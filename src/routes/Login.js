484163c588addf2cb5fd882fd614588c8f1ab1e2
howky <howeorb@naver.com>
2023-10-10 17:34:33 +0900

    로그인 디자인 수정

--- src/routes/Login.js
+++ src/routes/Login.js
@@ -1,13 +1,12 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import image1 from "../images/shine.png"
 import "../style/Login.css";
 import * as gvar from "../globalVar.js"
 /*백엔드에게 넘길떄 위 환경변수 삭제*/
 
 function Login() {
   const navigate = useNavigate();
-  
+  const [isFlipped, setIsFlipped] = useState(false);
   const [formDataLogin, setFormDataLogin] = useState({
     id: "",
     psword: "",
@@ -76,145 +75,98 @@ function Login() {
     }
   };
 
-    // 폼 제출 핸들러
-    async function handleSubmit(e) {
-      e.preventDefault();
+  // 폼 제출 핸들러
+  async function handleSubmit(e) {
+    e.preventDefault();
 
-      if (
-        formData.id === "" ||formData.psword === "" ||formData.confirm_psword === "" ||
-        formData.name === "" ||formData.email === "" ||formData.phone_num === "" ||
-        formData.adress === ""
-      ) {
-        // 어떤 입력란이라도 비어있는 경우
-        alert("모든 필드를 입력해");
-        return; // 함수 종료
-      }
-      console.log(formData);
-      // 정보 전달 함수
-      try {
-        const response = await fetch('api/register', {
-          credentials: 'include',
-          method: "POST",
-          headers: {
-            "Content-Type" : "application/json",
-          },
-          body: JSON.stringify(formData),
-        });
-  
-        if (response.ok) {
-          const res = await response.json();
-  
-          if(res.success) {
-            navigate("/Login");
-          } else {
-            alert(res.msg);
-          }
+    if (
+      formData.id === "" || formData.psword === "" || formData.confirm_psword === "" ||
+      formData.name === "" || formData.email === "" || formData.phone_num === "" ||
+      formData.adress === ""
+    ) {
+      // 어떤 입력란이라도 비어있는 경우
+      alert("모든 필드를 입력해");
+      return; // 함수 종료
+    }
+    console.log(formData);
+    // 정보 전달 함수
+    try {
+      const response = await fetch('api/register', {
+        credentials: 'include',
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        const res = await response.json();
+
+        if (res.success) {
+          navigate("/Login");
         } else {
-          throw Error("서버 응답 실패");
+          alert(res.msg);
         }
-        
-      } catch(err) {
-        console.error(Error('회원가입 중 에러 발생'));
+      } else {
+        throw Error("서버 응답 실패");
       }
-    };
+
+    } catch (err) {
+      console.error(Error('회원가입 중 에러 발생'));
+    }
+  };
 
   return (
     <div className="login">
-      <div className="box"></div>
-      
-      <div className="card-switch">
-        <label className="switch">
-          <input className="toggle" type="checkbox" />
-          <span className="slider"></span>
-          <img className="image" src={image1} alt="로그인 사진"/>
-          <span className="card-side"></span>
-          <div className="flip-card__inner">
-          <div className="flip-card__front">
-              <form onSubmit={handleSubmitLogin} className="flip-card__form">
-                <div className="input-group">
-                  <label htmlFor="userID">UserID</label>
-                  <input classname={`flip-card__input ${formDataLogin.id === "" ? "warning" : ""}`}
-                    type="text"id="id"name="id"placeholder="ID"
-                    value={formDataLogin.id}onChange={handleInputChangeLogin}
-                  />
-                  </div>
-                  <div className="input-group">
-                  <label htmlFor="password">Password</label>
-                  <input classname="flip-card__input"
-                    type="password"id="psword"name="psword"placeholder="PW"
-                    value={formDataLogin.psword}onChange={handleInputChangeLogin}
-                  />
-                  </div>
-                  <div className="input-group">
-                  <input type="submit" value="Login" />
-                  </div>
-              </form>
+      <div className="box" >
+        <div className={`left-box ${isFlipped ? "Activie" : ""}`}>
+          <form onSubmit={handleSubmitLogin} className="flip-card-front">
+            <label htmlFor="userID">UserID</label>
+            <input className="input-group"
+              type="text" id="id" name="id" placeholder="ID"
+              value={formDataLogin.id} onChange={handleInputChangeLogin} />
+            <label htmlFor="password">Password</label>
+            <input className="input-group"
+              type="password" id="psword" name="psword" placeholder="PW"
+              value={formDataLogin.psword} onChange={handleInputChangeLogin} />
+            <input className="buttonToLogin" type="submit" value="Login" />
+          </form>
+          <form onSubmit={handleSubmit} className="flip-card-back">
+            <label htmlFor="id">id</label>
+            <input className="input-group"
+              type="text" id="id" name="id" placeholder="ID"
+              value={formData.id} onChange={handleInputChange} />
+            <br />
+            <label htmlFor="psword">Password</label>
+            <input className="input-group"
+              type="text" id="psword" name="psword" placeholder="PW"
+              value={formData.psword} onChange={handleInputChange} />
+            <br />
+            <label htmlFor="confirm_psword">confirm</label>
+            <input className="input-group"
+              type="text" id="confirm_psword" name="confirm_psword" placeholder="confirm_psword"
+              value={formData.confirm_psword} onChange={handleInputChange} />
+            <br />
+            <label htmlFor="name">UserName</label>
+            <input className="input-group"
+              type="text" id="name" name="name" placeholder="Name"
+              value={formData.name} onChange={handleInputChange} />
+            <br />
+            <input className="buttonToSignUp" type="submit" value="Sign UP" onClick={handleSubmit} />
+          </form>
+        </div>
+        <div className="light">
+        </div>
+        <div className="right-box" onClick={() => setIsFlipped(!isFlipped)}>
+          <label className={`switch ${isFlipped ? "Activie" : ""}`} >
+            <div className="text">
+              <div className={`left-text`}>login</div>
+              <div className={`right-text`}>Sign UP</div>
             </div>
-          <div className="flip-card__back">
-              <form onSubmit={handleSubmit}>
-                <div className="input-group">
-                  <label htmlFor="id">id</label>
-                  <input
-                    type="text" id="id" name="id" placeholder="ID"
-                    value={formData.id} onChange={handleInputChange}
-                  />
-                  </div>
-                  <br />
-                  <div className="input-group">
-                  <label htmlFor="psword">Password</label>
-                  <input
-                    type="text" id="psword" name="psword" placeholder="PW"
-                    value={formData.psword} onChange={handleInputChange}
-                  />
-                  </div>
-                  <br />
-                  <div className="input-group">
-                  <label htmlFor="confirm_psword">confirm</label>
-                  <input
-                    type="text" id="confirm_psword" name="confirm_psword" placeholder="confirm_psword"
-                    value={formData.confirm_psword} onChange={handleInputChange}
-                  />
-                  </div>
-                  <br />
-                  <div className="input-group">
-                  <label htmlFor="name">UserName</label>
-                  <input
-                    type="text" id="name" name="name" placeholder="Name"
-                    value={formData.name} onChange={handleInputChange}
-                  />
-                  </div>
-                  <br />
-                  <div className="input-group">
-                  <label htmlFor="email">email</label>
-                  <input
-                    type="text" id="email" name="email" placeholder="email"
-                    value={formData.email} onChange={handleInputChange}
-                  />
-                  </div>
-                  <br />
-                  <div className="input-group">
-                  <label htmlFor="phone_num">phone number</label>
-                  <input
-                    type="text" id="phone_num" name="phone_num" placeholder="phoneNumber"
-                    value={formData.phone_num} onChange={handleInputChange}
-                  />
-                  </div>
-                  <br />
-                  <div className="input-group">
-                  <label htmlFor="adress">address</label>
-                  <input
-                    type="text" id="adress" name="adress" placeholder="address"
-                    value={formData.adress} onChange={handleInputChange}
-                  />
-                  </div>
-                  <br />
-                  <div className="input-group">
-                  <input type="submit" value="Sign UP" onClick={handleSubmit} />
-                </div>
-              </form>
-            </div>
-          </div>
-        </label>
+            <div className={`slider ${isFlipped ? "Activie" : ""}`}></div>
+          </label>
+        </div>
       </div>
     </div>
   );
