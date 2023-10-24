5bd9da9e7b63a83d27a6964e83d7230742c2d4cf
wnsghks <wnsghks0223@gmail.com>
2023-10-24 13:55:22 +0900

    상품 api연결

--- src/components/items.js
+++ src/components/items.js
@@ -4,14 +4,19 @@ import PropTypes from "prop-types";
 import "../style/item.css";
 import { useEffect, useState } from "react";
 
-function Items( {path, state} ) {
+function Items( props ) {
   // 불러온 데이터를 배열형태로 저장할 변수
-  // const [light, setLight] = useState([]);
-  const light = [1, 2, 3];
-  //주소창에 현재 램프 종류를 뛰우기 위한 과정들
-  let pathName = "";
-  if(path === "펜던트" || path === "플로어 램프" || path === "테이블 램프" || path === "월 램프")
-    pathName = path.replace(/ /g, ""); // 주소창 공백 제거를 위한 변수
+  const [light, setLight] = useState([]);
+
+  useEffect(() => {
+    setLight(props.data);
+  }, [props]);
+
+
+  // //주소창에 현재 램프 종류를 뛰우기 위한 과정들
+  // let pathName = "";
+  // if(path === "펜던트" || path === "플로어 램프" || path === "테이블 램프" || path === "월 램프")
+  //   pathName = path.replace(/ /g, ""); // 주소창 공백 제거를 위한 변수
   
   // // 이미지 데이터를 base64로 변환하여 반환하는 함수
   // function arrayBufferToBase64(buffer) {
@@ -25,22 +30,25 @@ function Items( {path, state} ) {
   // }
 
   // 렌더링 속도보다 가져오는 속도가 느리면 빈값이 뜨게 되므로 if조건문을 같이 작성 해줘야 오류가 발생하지 않는다.
+  // useEffect(() => {
+  //   if(state) {
+  //     setLight((state));
+  //   }
+  // }, [state]);
+
   useEffect(() => {
-      console.log(light);
-  }, [state]);
+    console.log(light);
+  }, [light]);
 
   return (
     <div className="products-items">
-      {light.map((light, index) => {
-        {/* const base64Image = arrayBufferToBase64(light.IMG_DATA.data); */}
-        return (
-          <Link className="item-box" to={`/Products/${pathName}/ㅗㅑㅗㅑㅁㄴㅇ`} key={index} state={""}>
-            <h1>hi</h1>
-            <p>goo</p>
-            {/* <img src={`data:image/png;base64,${base64Image}`} alt="" /> */}
-          </Link>
-        )
-      })}
+      {light !== "" && light.map((light, index) => (
+      <Link className="item-box" to={`/Products/ji/ㅗㅑ`} key={index} state={""}>
+        <h1>hi</h1>
+        <p>goo</p>
+        {/* <img src={`data:image/png;base64,${base64Image}`} alt="" /> */}
+      </Link>
+    ))}
     </div>
   );
 }
--- src/routes/Header.js
+++ src/routes/Header.js
@@ -3,70 +3,12 @@ import { Link } from "react-router-dom";
 import "../style/Header.css";
 import Light from "../components/light";
 import Search from "../components/search";
-import { useEffect, useState } from "react";
-// import * as gvar from "../globalVar.js";
-
 
 function Header() {
-  // 상품정보 저장
-  const [Lamps, setLamps] = useState({
-    category : "",
-    pageListSize : "",
-    page: "",
-  });
-
-  // 연결한 데이터를 저장하는 변수
-  const [data, setData] = useState("");
-
   // 조명일러스트 class변환함수
   const onReset = (e) => {
     e.target.parentElement.parentElement.parentElement.parentElement.firstChild.firstChild.className = "light 펜던트";
     e.target.parentElement.parentElement.parentElement.parentElement.firstChild.firstChild.firstChild.className = "light";
-  };
-
-  // 조명이름에 따라 카테고리 값 지정
-  function onCheck(e) {
-    if (e.target.innerText === "펜던트") {
-      setLamps({
-        category: 3,
-        pageListSize: 20,
-        page: 1,
-      });
-    }
-  };
-
-  // lamp값이 바뀌면 api요청
-  useEffect(() => {
-    onClickLamp();
-  },[Lamps]) 
-
-  // api 연결
-  async function onClickLamp() {
-    // 정보 전달 함수
-    try {
-      const response = await fetch('https://port-0-node-express-jvvy2blmegkftc.sel5.cloudtype.app/api/product/category', {
-        credentials: 'include',
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(Lamps),// Lamps 값을 JSON 문자열로 변환하여 요청 
-      });
-
-      // 연결 성공 유무 판단
-      if (response.ok) {
-        const res = await response.json();
-        if (res.success) {
-          setData(res.data); //데이터 저장
-        } else {
-          alert(res.msg);
-        }
-      } else {
-        throw Error("서버 응답 실패");
-      }
-    } catch (err) {
-      console.error(Error('불러오는 중 에러 발생'));
-    }
   };
 
   return (
@@ -88,8 +30,8 @@ function Header() {
         <div className="menu-bottom">
           <nav>
             {/* onclick이벤트 전에 state를 전달하면 빈값으로 전달되기에 순서지켜주기 */}
-            {/* 또한 과거버전은 to에 state를 썼지만 v6부터는 따로 써야한다고 한다. */}
-            <span><Link className="menu-category-item" to={"/Products/펜던트"} onClick={onCheck} state={{data: data}}>펜던트</Link></span> 
+            {/* 또한 과거버전은 to에 state를 썼지만 현재는 따로 써야한다고 한다. */}
+            <span><Link className="menu-category-item" to={"/Products/펜던트"}>펜던트</Link></span> 
             <span><Link className="menu-category-item" to={"/Products/플로어램프"}>플로어 램프</Link></span>
             <span><Link className="menu-category-item" to={"/Products/테이블램프"}>테이블 램프</Link></span>
             <span><Link className="menu-category-item" to={"/Products/월램프"}>월 램프</Link></span>
